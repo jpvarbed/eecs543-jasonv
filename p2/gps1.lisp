439623f95fdadf11f0a99df255b65c82b079e06b
jpvarbed <jpvarbed@f01b1396-d6ae-f755-8479-8eae1681d78f>
2012-09-28 04:55:36 +0000

    added find-all to gps1

--- p2/gps1.lisp
+++ p2/gps1.lisp
@@ -4,6 +4,17 @@
 
 ;;;; File gps1.lisp: First version of GPS (General Problem Solver)
 
+(defun find-all (item sequence &rest keyword-args
+                 &key (test #'eql) test-not &allow-other-keys)
+  "Find all those elements of sequence that match item,
+  according to the keywords.  Doesn't alter sequence."
+  (if test-not
+      (apply #'remove item sequence 
+             :test-not (complement test-not) keyword-args)
+      (apply #'remove item sequence
+             :test (complement test) keyword-args)))
+
+
 (defvar *state* nil "The current state: a list of conditions.")
 
 (defvar *ops* nil "A list of available operators.")
