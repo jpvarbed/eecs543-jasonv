28e599e148aef6440f424fffae1d398464622b0c
jpvarbed <jpvarbed@f01b1396-d6ae-f755-8479-8eae1681d78f>
2012-09-29 02:19:40 +0000

    committed file

--- trunk/p2/gps1.lisp
+++ trunk/p2/gps1.lisp
@@ -4,16 +4,6 @@
 
 ;;;; File gps1.lisp: First version of GPS (General Problem Solver)
 
-(defun find-all (item sequence &rest keyword-args
-                 &key (test #'eql) test-not &allow-other-keys)
-  "Find all those elements of sequence that match item,
-  according to the keywords.  Doesn't alter sequence."
-  (if test-not
-      (apply #'remove item sequence 
-             :test-not (complement test-not) keyword-args)
-      (apply #'remove item sequence
-             :test (complement test) keyword-args)))
-
 
 (defvar *state* nil "The current state: a list of conditions.")
 
